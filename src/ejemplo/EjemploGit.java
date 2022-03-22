93c93b46a9a92bbc252ba19be02087c150bf05ac
LuisanaRC <101008848+LuisanaRC@users.noreply.github.com>
2022-03-22 15:46:02 +0100

    Incluyendo ficheros datos

--- src/ejemplo/EjemploGit.java
+++ src/ejemplo/EjemploGit.java
@@ -1,10 +1,12 @@
 package ejemplo;
+import java.io.*;
 
 public class EjemploGit {
 
 	public static void main(String[] args) {
 		// Más moficicaciones
 		// Usamos la variable
+		File f = new File("datos.txt");
 		int val = 0;
 		System.out.println("Hola mundo!!"+val);
 		
